b303b5fd9ff63ff9c92ea4c564ef1d20354ae227
sreekanth <sreekanth.koye@gmail.com>
2019-05-29 13:56:08 +0530

    Added class Sample.java

---
+++ TestProject/src/org/test/Sample.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Sample {
+	public void id() {
+		System.out.println("1234");
+
+	}
+	public void name() {
+		System.out.println("Bharath");
+
+	}
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.id();
+		s.name();
+	}
+
+}
