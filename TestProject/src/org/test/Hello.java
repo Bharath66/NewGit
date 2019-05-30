6c4992845a8af216e00d8c624144265dfd9fb149
Bharath <kumarbharath410@gmai.com>
2019-05-30 13:45:25 +0530

    djksfsfbjf

---
+++ TestProject/src/org/test/Hello.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Hello {
+	public void sal() {
+		System.out.println("1234.89");
+	}
+	public static void main(String[] args) {
+		Hello h=new Hello();
+		h.sal();
+	}
+
+}
